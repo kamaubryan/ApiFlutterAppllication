163737fc88f0d508d162beea05f0849926dc64e8
kamaubryan <briankamau344@gmail.com>
2024-12-17 16:09:00 +0300

    this is a pages where the products are displayed

---
+++ lib/allproducts.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class Allproducts extends StatelessWidget {
+  const Allproducts({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
